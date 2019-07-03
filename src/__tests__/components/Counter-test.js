f98937429910b003c4c31d70d44876027eab8ab2
Tia Rose <shortyshort789@aol.com>
2019-07-02 20:40:36 -0700

    added dependency for travis

--- src/__tests__/components/Counter-test.js
+++ src/__tests__/components/Counter-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import Counter from '../../components/Counter/Counter';
 
 describe('Counter component', () => {
@@ -8,5 +8,44 @@ describe('Counter component', () => {
     const app = shallow(<Counter />);
 
     expect(app.find('.count').text()).toBe('0');
+  });
+
+  describe('Counter component', () => {
+    it('has an initial state', () => {
+      // eslint-disable-next-line no-undef
+      const app = shallow(<Counter />);
+  
+      expect(app.find('.count').text()).toBe('0');
+    });
+  
+    it('can increment by 1', () => {
+      // eslint-disable-next-line no-undef
+      const wrapper = mount(<Counter/>);
+  
+      wrapper.find('.increment').simulate('click');
+      expect(wrapper.state('counter')).toBe(1);
+  
+      wrapper.find('.increment').simulate('click');
+      expect(wrapper.state('counter')).toBe(2);
+    });
+  
+    it('can decrement by 1', () => {
+      // eslint-disable-next-line no-undef
+      const wrapper = mount(<Counter/>);
+  
+      wrapper.find('.decrement').simulate('click');
+      expect(wrapper.state('counter')).toBe(-1);
+  
+      wrapper.find('.decrement').simulate('click');
+      expect(wrapper.state('counter')).toBe(-2);
+    });
+  });
+  
+  describe('Counter snapshot test', () => {
+    it('renders to DOM correctly', () => {
+      const component = renderer.create(<Counter />);
+      const tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
